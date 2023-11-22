c2764a55595ba908010c01c114e9d43a948b979c
LENOVO <LENOVO@DESKTOP-EAHHS5B>
2023-11-21 22:34:20 -0800

    FirstCommit

---
+++ JenkinsDemo/src/test/java/TestCases/TC4test.java
@@ -0,0 +1,13 @@
+package TestCases;
+
+import org.testng.annotations.Test;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TC4test {
+	@Test
+	public void m4()
+	{
+		Reporter.log("test working",true);
+	}
+}
